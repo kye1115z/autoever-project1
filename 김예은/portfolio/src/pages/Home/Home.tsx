0e8acbb273ba35ad0a79e2c8dd6d12b7858a3b70
kye1115z <kye1115z@naver.com>
2026-02-25 16:31:30 +0900

    추가: 스크롤뷰 구현

--- 김예은/portfolio/src/pages/Home/Home.tsx
+++ 김예은/portfolio/src/pages/Home/Home.tsx
@@ -6,9 +6,19 @@ import styles from "./Home.module.css";
 const Home = () => {
   return (
     <div className={styles.home}>
-      <AboutMe />
-      <Career />
-      <Posts />
+      <section id="about" className={styles.section}>
+        <AboutMe />
+      </section>
+      <section id="project" className={styles.section}>
+        <Career />
+      </section>
+      <section id="blog" className={styles.section}>
+        <Posts />
+      </section>
+      <section id="contacts" className={styles.section}>
+        <h2>Contacts</h2>
+        <p>연락처 정보를 여기에 추가하세요.</p>
+      </section>
     </div>
   );
 };
