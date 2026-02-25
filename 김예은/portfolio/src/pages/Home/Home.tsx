3fd0581105d74675572abcdfa3176b6eb0b0da66
kye1115z <kye1115z@naver.com>
2026-02-25 11:58:00 +0900

    추가: Posts .tsx, .module.css 파일 생성

--- 김예은/portfolio/src/pages/Home/Home.tsx
+++ 김예은/portfolio/src/pages/Home/Home.tsx
@@ -1,5 +1,6 @@
 import AboutMe from "../AbooutMe/AboutMe";
 import Career from "../Career/Career";
+import Posts from "../Posts/Posts";
 import styles from "./Home.module.css";
 
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
     <div className={styles.home}>
       <AboutMe />
       <Career />
+      <Posts />
     </div>
   );
 };
---
+++ 김예은/portfolio/src/pages/Posts/Posts.tsx
@@ -0,0 +1,9 @@
+const Posts = () => {
+  return (
+    <div>
+      <h1>Study Note</h1>
+    </div>
+  );
+};
+
+export default Posts;
