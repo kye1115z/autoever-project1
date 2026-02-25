956b43b05484022806c1d683a1d394bc2a859829
kye1115z <kye1115z@naver.com>
2026-02-25 12:35:32 +0900

    추가: Posts 페이지 supabase에서 데이터 받아오기

--- src/pages/Posts/Posts.tsx
+++ src/pages/Posts/Posts.tsx
@@ -1,7 +1,41 @@
+import styles from "./Posts.module.css";
+import { useEffect, useState } from "react";
+import type { Post } from "../../types";
+import { supabase } from "../../api/supabase";
+
 const Posts = () => {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const { data, error } = await supabase
+        .from("posts")
+        .select("*")
+        .eq("type", "TIL"); // TIL 타입의 게시물만 가져오도록
+
+      if (error) {
+        console.log("error: ", error);
+      } else {
+        setPosts(data ?? []);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
   return (
-    <div>
-      <h1>Study Note</h1>
+    <div className={styles.posts}>
+      {posts &&
+        posts.map((post) => (
+          <div key={post.id} className={styles.post}>
+            <h1>Studay Note</h1>
+            <div className={styles.postContent}>
+              <p>{post.title}</p>
+              <p>{post.summary}</p>
+              <button>→ Read More</button>
+            </div>
+          </div>
+        ))}
     </div>
   );
 };
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
+// 사용자 프로필 타입 정의
 export interface AdminProfile {
   id: number;
   name: string;
@@ -6,4 +7,17 @@ export interface AdminProfile {
   email: string | null;
   phone: string | null;
   created_at: string;
+}
+
+// 게시물(학습 내용 + TIL) 타입 정의
+export type PostType = "BLOG" | "TIL";
+
+export interface Post {
+  id: number;
+  title: string;
+  slug: string;
+  summary: string;
+  type: PostType;
+  views?: number;
+  created_at: string | Date;
 }
