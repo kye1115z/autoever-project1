8dfd59a0702d9764c0d13f9398dea1edf013e1f2
kye1115z <kye1115z@naver.com>
2026-02-25 11:42:50 +0900

    추가: Career .tsx, .module.css 추가

--- 김예은/portfolio/src/pages/Home/Home.tsx
+++ 김예은/portfolio/src/pages/Home/Home.tsx
@@ -1,10 +1,12 @@
 import AboutMe from "../AbooutMe/AboutMe";
+import Career from "../Career/Career";
 import styles from "./Home.module.css";
 
 const Home = () => {
   return (
     <div className={styles.home}>
       <AboutMe />
+      <Career />
     </div>
   );
 };
