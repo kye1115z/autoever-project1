6d70ab33b5b8d2a6aa975c8c804a59c05e249a17
kye1115z <kye1115z@naver.com>
2026-02-25 10:43:45 +0900

    추가: router 기능 추가

--- 김예은/portfolio/src/App.tsx
+++ 김예은/portfolio/src/App.tsx
@@ -1,9 +1,18 @@
-import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import styles from "./App.module.css";
+
+import Header from "./components/Header/Header";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
-    <div className="app">
-      <h1>김예은의 포트폴리오</h1>
+    <div className={styles.app}>
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
