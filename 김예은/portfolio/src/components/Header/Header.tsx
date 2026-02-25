4478e0235a3ed0a9cc33105e3ef8e88c2010b6ee
kye1115z <kye1115z@naver.com>
2026-02-25 10:42:16 +0900

    추가: Header .tsx, .module.css 파일 추가

---
+++ 김예은/portfolio/src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import styles from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <h1>김예은의 포트폴리오</h1>
+    </header>
+  );
+};
+
+export default Header;
