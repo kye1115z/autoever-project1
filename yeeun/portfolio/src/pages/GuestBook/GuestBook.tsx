ae95b6182c7cf4708e87b99a9e09de8ec4f77b57
YeEun Kim <kye1115z@naver.com>
2026-03-04 13:59:20 +0900

    추가: guestbook

--- yeeun/portfolio/src/pages/GuestBook/GuestBook.tsx
+++ yeeun/portfolio/src/pages/GuestBook/GuestBook.tsx
@@ -1,5 +1,12 @@
+import styles from "./GuestBook.module.css";
+
 const GuestBook = () => {
-  return <div>GuestBook</div>;
+  return (
+    <div className={styles.guestbook}>
+      <p>준비 중인 서비스입니다ㅠㅠ</p>
+      <img src="/assets/images/logo.png" alt="logo" />
+    </div>
+  );
 };
 
 export default GuestBook;
